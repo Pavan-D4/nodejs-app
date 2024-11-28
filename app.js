2c10d1a275d5b6c34c5ddf1daad23b25e4fab2a9
Pavan <68982480+Pavan-D4@users.noreply.github.com>
2024-11-28 23:17:32 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+// Simple Node.js Hello World App
+const http = require('http');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World from Node.js!\n');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
